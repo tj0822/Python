f6ac5a9859c74c724df8f060b8bf2d11827f0209
tj0822 <tj820822@gmail.com>
2018-02-20 23:34:07 +0900

    push

--- RPG/MoneyBot/SelctStocks.py
+++ RPG/MoneyBot/SelctStocks.py
@@ -1,11 +1,12 @@
 #-*- coding:utf-8 -*-
 import RPG.MoneyBot.MySQL as sql
-#
+import datetime
+
 def GetPortfolio(self):
     return {'006400'}
 
 def GetStockByAibrilScore(date = None):
-    sql = " SELECT AA.STOCK_CODE, AA.sentiment_targets, AA.sentiment_document, AA.RANK " \
+    query = " SELECT AA.STOCK_CODE, AA.sentiment_targets, AA.sentiment_document, AA.RANK " \
           "   FROM ( SELECT A.*, @curRank := @curRank + 1 AS RANK " \
           "            FROM (SELECT STOCK_CODE, AVG(sentiment_targets) sentiment_targets, AVG(sentiment_document) sentiment_document " \
           "                    FROM aibril_alu " \
@@ -13,6 +14,10 @@ def GetStockByAibrilScore(date = None):
           "                     AND issueDatetime < '%s' 	" \
           "                GROUP BY STOCK_CODE) A, (SELECT @curRank := 0) r O" \
           "                RDER BY A.sentiment_targets DESC) AA" \
-          " WHERE AA.RANK <= 1 " %(date, date)
-    print(sql)
+          " WHERE AA.RANK <= 1 " %(str(date), str(date + datetime.timedelta(days=1)))
+    print(query)
+    print(sql.selectStmt(query))
+
+
+GetStockByAibrilScore(datetime.datetime.strptime(str('2017-01-01'), "%Y-%m-%d").date())
 
