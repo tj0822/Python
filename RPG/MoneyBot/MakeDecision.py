45b26aec83c9c30b736c53dd28047f3cd57261d1
tj0822 <tj820822@gmail.com>
2018-02-20 22:30:30 +0900

    push

--- RPG/MoneyBot/MakeDecision.py
+++ RPG/MoneyBot/MakeDecision.py
@@ -20,39 +20,24 @@ def GetAbrilALUscoreByNews(stock, stockName, date):
 
     return decisionCode, tradeRate
 
-def GetAbrilAluScore(stock, stockName, date):
+def GetAbrilAluSumAvg(stock, stockName, date):
     fromDate = pd.to_datetime(str(date) + ' 15:30:00') + datetime.timedelta(days=-1)
     toDate = pd.to_datetime(str(date) + ' 15:30:00')
-    query = "SELECT SUM(sentiment_targets), SUM(sentiment_document) FROM aibril_alu WHERE STOCK_CODE = '%s' and issueDatetime >= '%s' and issueDatetime < '%s' " % (
-    stock, fromDate, toDate)
+    query = "SELECT SUM(sentiment_targets), SUM(sentiment_document), AVG(sentiment_targets), AVG(sentiment_document) FROM aibril_alu WHERE STOCK_CODE = '%s' and issueDatetime >= '%s' and issueDatetime < '%s' " % (stock, fromDate, toDate)
     result = sql.selectStmt(query)
 
     return result
 
-def GetAbrilAluScoreAvg(stock, stockName, date):
-    result = GetAbrilAluScore(stock, stockName, date)
+def GetAbrilAluScore(stock, stockName, date, flag):
+    result = GetAbrilAluSumAvg(stock, stockName, date)
     decisionCode = 0
     score = 0
 
     if result[0][0] is not None and result[0][1] is not None:
-        score = (float(result[0][0]) + float(result[0][1])) / 2
-
-    if score > 0:
-        decisionCode = 1
-    elif score < 0:
-        decisionCode = -1
-    else:
-        pass
-
-    return decisionCode, score
-
-def GetAbrilAluScoreSum(stock, stockName, date):
-    result = GetAbrilAluScore(stock, stockName, date)
-    decisionCode = 0
-    score = 0
-
-    if result[0][0] is not None and result[0][1] is not None:
-        score = (float(result[0][0]) + float(result[0][1])) / 2
+        if flag == 1:
+            score = (float(result[0][0]) + float(result[0][1])) / 2
+        elif flag == 0:
+            score = (float(result[0][2]) + float(result[0][3])) / 2
 
     if score > 0:
         decisionCode = 1
